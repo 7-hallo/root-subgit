4bf35d98c0017922a17726e3d1c18b93d37da30e
7zism3 <7zism3@gmail.com>
2024-07-25 17:35:07 +0700

    #setup - test

--- start.sh
+++ start.sh
@@ -7,7 +7,8 @@ fi
 
 # Chạy Docker Compose cho các file
 docker-compose -f other/keycloak/docker-compose.yml up -d
-docker-compose -f other/redis/docker-compose.yml up -d
+docker-compose -f other/nexus/docker-compose.yml up -d
 docker-compose -f other/postgres/docker-compose.yml up -d
+docker-compose -f other/redis/docker-compose.yml up -d
 
 echo 'Started!'
