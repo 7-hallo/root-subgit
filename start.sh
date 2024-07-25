8e7dcbc7d9de68da3cf4881ca1f558cd546e1b09
7zism3 <7zism3@gmail.com>
2024-07-25 14:13:47 +0700

    #setup - test

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Kiểm tra nếu mạng 7_hallo chưa tồn tại, thì tạo nó
+if ! docker network ls | grep -q "7_hallo"; then
+  docker network create 7_hallo
+fi
+
+# Chạy Docker Compose cho cả hai file
+docker-compose -f other/keycloak/docker-compose.yml up -d
+docker-compose -f other/redis/docker-compose.yml up -d
+docker-compose -f other/postgres/docker-compose.yml up -d
+
+echo 'Started!'
