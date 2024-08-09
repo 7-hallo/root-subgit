3c7ad8fe7049d5c920fb2e55163df99969c55b0c
7zism3 <7zism3@gmail.com>
2024-08-09 18:04:35 +0700

    #setup - add init.sh and remove .gitmodules

---
+++ init.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Clone repositories
+git clone https://github.com/7-hallo/center-config.git
+git clone https://github.com/7-hallo/common.git
+git clone https://github.com/7-hallo/config-repo.git
+git clone https://github.com/7-hallo/discovery.git
+git clone https://github.com/7-hallo/gateway.git
+git clone https://github.com/7-hallo/other.git
+git clone https://github.com/7-hallo/product-service.git
+git clone https://github.com/7-hallo/security.git
+
+echo "All repositories have been cloned."
